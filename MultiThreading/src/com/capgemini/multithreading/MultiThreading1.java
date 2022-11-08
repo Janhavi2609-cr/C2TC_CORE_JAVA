38bee44bed3d9117517876fca54aa60af417a642
JANHAVI KOLTE <JANHAVI KOLTE@LAPTOP-UIDEHH6R>
2022-11-08 11:22:53 +0530

    first commit

---
+++ MultiThreading/src/com/capgemini/multithreading/MultiThreading1.java
@@ -0,0 +1,25 @@
+package com.capgemini.multithreading;
+
+class Eclipse extends Thread{
+	
+}
+
+class Calculator{
+	
+}
+class Chrome{
+	
+}
+class OneNote{
+	
+}
+public class MultiThreading1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Eclipse e = new Eclipse();
+		e.start();
+		
+	}
+
+}
